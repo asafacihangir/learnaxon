5014cf509dc2e4720a0504d4a7067f60c7de904d
Vijay Nair <vijaynair@Vijays-MacBook-Pro-2.local>
2020-02-04 13:51:06 -0800

    Deletion of Idea files

--- tooling/simpletest/src/main/java/com/simpletest/coreapi/events.kt
+++
@@ -1,4 +0,0 @@
-package com.simpletest.coreapi
-
-data class SimpleEventCreated(val commandId: String, val commandData: String)
-data class SimpleEventModified(val commandId: String, val commandData : String)
--- tooling/simpletest/src/main/java/com/simpletest/upcasters/SimpleUpcaster.java
+++
@@ -1,7 +0,0 @@
-package com.simpletest.saga;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SimpleUpcaster{
-}
